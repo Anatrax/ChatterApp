66ca12b7eddfd518ddbc79f3f17e32de1f43ccde
Anatrax <villegas.samuel98@gmail.com>
2020-03-25 14:41:11 -0700

    Add sample tests

---
+++ client/tests/ControllerTester/ControllerTester.cpp
@@ -0,0 +1,20 @@
+
+//Bring in unit testing code
+#include "../Catch2/single_include/catch2/catch.hpp"
+
+//Include your .h files
+#include <iostream>
+
+using namespace std;
+
+TEST_CASE("Sample Test") {
+    cout << "Sample Test Case" << endl;
+
+    SECTION( "Section 1" ) {
+        REQUIRE(false == true);
+    }
+
+    SECTION( "Section 2" ) {
+        REQUIRE(1 == 1);
+    }
+}
